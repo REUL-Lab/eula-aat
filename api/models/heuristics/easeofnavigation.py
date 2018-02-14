a227d715b0a95d12d4fc1a45d78cf50a1a15739d
kmaster2520 <sathvik.kadaveru@gmail.com>
2018-02-14 14:01:15 -0500

    basic toc

--- api/models/heuristics/easeofnavigation.py
+++ api/models/heuristics/easeofnavigation.py
@@ -1,7 +1,31 @@
 from models.heuristic import Heuristic
+from boilerpipe.extract import Extractor
 
 # Formal 3
 # Ensure ease of user navigation.
 class EaseOfNavigation(Heuristic):
     def score(self, eula):
-        return {'score': -1, 'max': 4, 'reason': 'Not implemented'}
+        extractor = Extractor(extractor='KeepEverythingExtractor', html= eula.html)
+        text = extractor.getText()
+        text1000 = text[:1000]
+        tocscore = 0
+        reason = ''
+        indexOfTable = -1
+
+        tocindicators = ['TABLE OF CONTENTS', 'Table Of Contents']
+
+        for ind in tocindicators:
+            if indexOfTable < 0:
+                indexOfTable = text1000.find('TABLE OF CONTENTS')
+            else:
+                break
+        
+        if indexOfTable < 0:
+            tocscore = 0
+            reason += 'No table of contents found near beginning of EULA. '
+        else:
+            tocscore = 4
+            reason += 'Found table of contents. '
+
+
+        return {'score': tocscore, 'max': 4, 'reason': reason, 'text': text1000}
