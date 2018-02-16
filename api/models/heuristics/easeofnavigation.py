dd1f2a8914d4a90ca3d7145d8af9b546c7cf98da
Mark <mclayton@gatech.edu>
2018-02-16 13:59:01 -0500

    Changed naming conventions to match project, reduced redunant lines, added processing for non-html eulas, and error handled for html only eulas.

--- api/models/heuristics/easeofnavigation.py
+++ api/models/heuristics/easeofnavigation.py
@@ -6,33 +6,31 @@ from boilerpipe.extract import Extractor
 class EaseOfNavigation(Heuristic):
     def score(self, eula):
 
-        # gets actual text since eula.text doesn't get everything
-        extractor = Extractor(extractor='KeepEverythingExtractor', html= eula.html)
-        text = extractor.getText()
+        if eula.html is not None:
+            # gets actual text since eula.text doesn't get everything
+            extractor = Extractor(extractor='KeepEverythingExtractor', html= eula.html)
+            text = extractor.getText()
+        else:
+            text = eula.text
 
         # limit text to first 1000 characters
-        text1000 = text[:1000]
+        text_first_1000 = text[:1000]
         tocscore = 0 # score for table of contents
         reason = '' # reason
-        indexOfTable = -1 # index of matching text
+        index_of_table = -1 # index of matching text
 
         # text to look out for
         tocindicators = ['TABLE OF CONTENTS', 'Table Of Contents']
 
         # find index of indicator text
         for ind in tocindicators:
-            if indexOfTable < 0:
-                indexOfTable = text1000.find('TABLE OF CONTENTS')
+            if index_of_table < 0:
+                index_of_table = text_first_1000.find('TABLE OF CONTENTS')
             else:
                 break
-        
-        # indexOfTable < 0 --> not found
-        if indexOfTable < 0:
-            tocscore = 0
-            reason += 'No table of contents found near beginning of EULA. '
-        else:
-            tocscore = 4
-            reason += 'Found table of contents. '
 
+        # Score of 4 if found, score of 0 if not
+        # TODO: additional scoring hyperlinked vs unlinked table
+        tocscore = 4 if index_of_table >= 0 else 0
 
-        return {'score': tocscore, 'max': 4, 'reason': reason, 'text': text1000}
+        return {'score': tocscore, 'max': 4, 'index': index_of_table}
