7ac1a3f63ce4b3943d98721e693820e2d83c4321
davidjtran <david.tran456@gmail.com>
2018-02-19 13:08:43 -0500

    Added more data formats, cleaned up code

--- api/models/heuristics/notifychangesinpolicy.py
+++ api/models/heuristics/notifychangesinpolicy.py
@@ -1,24 +1,28 @@
-from models.heuristic import Heuristic
+# from models.heuristic import Heuristic
 import re
 
-class NotifyChangesInPolicy(Heuristic):
+class NotifyChangesInPolicy():
     def score(self, eula):
         text = eula.text
-        wordlist = ["last modified", "last updated", "last changed"]
-        for word in wordlist:
-            if (word in text.lower()):
-                return {'score': 4, 'max': 4, 'found notification': True}
 
-        match = re.search(r'(updated|modified|changed)\W\s*(\d+/\d+/\d+)', text, re.IGNORECASE)
-
-        if (match != None):
+        if re.search(r'(updated|modified|changed)\W*\s*(\d+\W\d+\W\d+)', text, re.IGNORECASE):
             return {'score': 4, 'max': 4, 'found notification': True}
 
-        match = re.search(r'(updated|modified|changed)\W\s*((January|February|March|April|May|June|July'
-                          r'|August|September|October|November|December)+\s*\d+\W\s*\d)',
-                          text, re.IGNORECASE)
+        elif re.search(r'(updated|modified|changed)\W*\s*(January|February|March|April|May|June|July'
+                       r'|August|September|October|November|December)+\s*\d+\W*\s*\d', text, re.IGNORECASE):
+            return {'score': 4, 'max': 4, 'found notification': True}
 
-        if (match != None):
+        elif re.search(r'(updated|modified|changed)\W*(Jan|Feb|Mar|Apr|May|Jun|Jul'
+                       r'|Aug|Sep|Oct|Nov|Dec)+\W*\d+\W*\d', text, re.IGNORECASE):
+            return {'score': 4, 'max': 4, 'found notification': True}
+
+        elif re.search(r'(updated|modified|changed)\W*(\d\W*)+(January|February|March|April|May|June|July'
+                       r'|August|September|October|November|December)+\W*\d', text, re.IGNORECASE):
+            return {'score': 4, 'max': 4, 'found notification': True}
+
+        elif re.search(r'(updated|modified|changed)\W*(\d\W*)+(Jan|Feb|Mar|Apr|May|Jun|Jul'
+                       r'|Aug|Sep|Oct|Nov|Dec)+\W*\d',text, re.IGNORECASE):
             return {'score': 4, 'max': 4, 'found notification': True}
 
         return {'score': 0, 'max': 4, 'found notification': False}
+
