5a278c66267a9fe76360928dcbf9fd951c0afa30
davidjtran <david.tran456@gmail.com>
2018-02-19 18:48:35 -0500

    Missing import and added comments

--- api/models/heuristics/notifychangesinpolicy.py
+++ api/models/heuristics/notifychangesinpolicy.py
@@ -1,28 +1,32 @@
-# from models.heuristic import Heuristic
+from models.heuristic import Heuristic
 import re
 
-class NotifyChangesInPolicy():
+#procedural 7a
+
+class NotifyChangesInPolicy(Heuristic):
     def score(self, eula):
         text = eula.text
 
+        #date format: any number formatting 26-1-2011 or 1/26/2011
         if re.search(r'(updated|modified|changed)\W*\s*(\d+\W\d+\W\d+)', text, re.IGNORECASE):
             return {'score': 4, 'max': 4, 'found notification': True}
-
+        #date format: January 26, 2011
         elif re.search(r'(updated|modified|changed)\W*\s*(January|February|March|April|May|June|July'
                        r'|August|September|October|November|December)+\s*\d+\W*\s*\d', text, re.IGNORECASE):
             return {'score': 4, 'max': 4, 'found notification': True}
-
+        #date format: Jan. 26, 2011
         elif re.search(r'(updated|modified|changed)\W*(Jan|Feb|Mar|Apr|May|Jun|Jul'
                        r'|Aug|Sep|Oct|Nov|Dec)+\W*\d+\W*\d', text, re.IGNORECASE):
             return {'score': 4, 'max': 4, 'found notification': True}
-
+        #date format: 26 January 2011
         elif re.search(r'(updated|modified|changed)\W*(\d\W*)+(January|February|March|April|May|June|July'
                        r'|August|September|October|November|December)+\W*\d', text, re.IGNORECASE):
             return {'score': 4, 'max': 4, 'found notification': True}
-
+        #date format: 26 Jan. 2011
         elif re.search(r'(updated|modified|changed)\W*(\d\W*)+(Jan|Feb|Mar|Apr|May|Jun|Jul'
                        r'|Aug|Sep|Oct|Nov|Dec)+\W*\d',text, re.IGNORECASE):
             return {'score': 4, 'max': 4, 'found notification': True}
 
         return {'score': 0, 'max': 4, 'found notification': False}
+
 
